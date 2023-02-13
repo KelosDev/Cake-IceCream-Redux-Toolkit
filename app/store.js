406976768d7b217128059c78c9926f207e1320aa
KelosDev <gpiraswebdev@gmail.com>
2023-02-13 14:14:49 +0100

    update

--- app/store.js
+++ app/store.js
@@ -2,13 +2,15 @@ const configureStore = require('@reduxjs/toolkit').configureStore
 const cakeReducer = require('../features/cake/cakeSlice')
 const icecreamReducer = require('../features/iceCream/icecreamSlice')
 const reduxLogger = require('redux-logger')
+const userReducer = require('../features/user/userSlice')
 
 const logger = reduxLogger.createLogger()
 
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
-        icecream: icecreamReducer
+        icecream: icecreamReducer,
+        user: userReducer
     },
     //middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger), // lo faccio perchè per default la funzione configureStore aggiunge alcuni middleware a redux store automaticamente
 })
