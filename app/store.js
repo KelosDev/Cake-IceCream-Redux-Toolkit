b338b0c012ab73064c4abafeafd1a985588fe66b
KelosDev <gpiraswebdev@gmail.com>
2023-02-13 11:24:30 +0100

    update

--- app/store.js
+++ app/store.js
@@ -1,9 +1,11 @@
 const configureStore = require('@reduxjs/toolkit').configureStore
 const cakeReducer = require('../features/cake/cakeSlice')
+const icecreamReducer = require('../features/iceCream/icecreamSlice')
 
 const store = configureStore({
     reducer: {
-        cake: cakeReducer
+        cake: cakeReducer,
+        icecream: icecreamReducer
     }
 })
 
